8fedf1aed07e17d1bbfb8b55fa26a6c121e38330
GodWithegg <675175647@qq.com>
2019-04-23 21:41:03 +0800

    sql实践

---
+++ sql_learn/join.sql
@@ -0,0 +1,12 @@
+create table A(
+	id int,
+	name varchar(10)
+);
+
+create table B(
+	id int,
+	name varchar(10)
+);
+
+insert into A values(1,'a'),(2,'b'),(3,'c');
+insert into B values(2,'d'),(3,'c'),(4,'d');
