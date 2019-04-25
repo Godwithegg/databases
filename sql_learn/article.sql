c78e5f57fa745037f4e03dd6e1dd078e02b4dd86
GodWithegg <675175647@qq.com>
2019-04-26 00:01:32 +0800

    article表简历

---
+++ sql_learn/article.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS `article`(
+`id` INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+`author_id` INT(10) UNSIGNED NOT NULL,
+`category_id` INT(10) UNSIGNED NOT NULL,
+`views` INT(10) UNSIGNED NOT NULL,
+`comments` INT(10) UNSIGNED NOT NULL,
+`title` VARCHAR(255) NOT NULL,
+`content` TEXT NOT NULL
+);
+
+INSERT INTO `article`(`author_id`,`ategory_id`,`views`,`comments`,`title`,`content`) VALUES
+(1,1,1,1,'1','1'),
+(2,2,2,2,'2','2'),
+(1,1,3,3,'3','3');
